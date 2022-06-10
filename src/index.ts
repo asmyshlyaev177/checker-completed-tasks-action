f99fe3e79e8adf0fb1215c6b244920956696cc2c
Aleksandr Smyshlyaev <asmyshlyaev177@gmail.com>
2022-06-10 21:12:25 +0400

    trying return checks instead of throwing an error

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 import {removeIgnoreTaskLitsText, createTaskListText} from './utils'
 
-async function run(): Promise<void> {
+async function run() {
   try {
     const body = github.context.payload.pull_request?.body
 
@@ -14,11 +14,9 @@ async function run(): Promise<void> {
       core.info('no task list and skip the process.')
       await githubApi.checks.create({
         name: appName,
-        // eslint-disable-next-line @typescript-eslint/camelcase
         head_sha: github.context.payload.pull_request?.head.sha,
         status: 'completed',
         conclusion: 'success',
-        // eslint-disable-next-line @typescript-eslint/camelcase
         completed_at: new Date().toISOString(),
         output: {
           title: appName,
@@ -36,34 +34,47 @@ async function run(): Promise<void> {
     core.debug('creates a list of tasks which removed ignored task: ')
     core.debug(result)
 
-    const isTaskCompleted = result.match(/(- \[[ ]\].+)/g) === null
+    const uncompletedTasks = result.match(/(- \[[ ]\].+)/g)
+    const completedTasks = result.match(/(- \[[x]\].+)/g)
+    // const isTaskCompleted = result.match(/(- \[[ ]\].+)/g) === null
+    const isTaskCompleted = !uncompletedTasks
 
     const text = createTaskListText(result)
 
     core.debug('creates a list of completed tasks and uncompleted tasks: ')
     core.debug(text)
+    core.debug('COMPLETED ' + completedTasks)
+    core.debug('UNCOMPLETED ' + uncompletedTasks)
+    core.debug('isTaskCompleted ' + isTaskCompleted)
+    const summary = `Completed tasks:
+    ${(completedTasks && `${completedTasks}`) || 'Nothing....'};
+    
+    Uncompleted tasks:
+    ${(uncompletedTasks && `${uncompletedTasks}`) || 'All done!'}`
+    core.debug('SUMMARY ' + summary)
 
-    await githubApi.checks.create({
+    const conclusion = isTaskCompleted ? 'success' : 'failure'
+    return await githubApi.checks.create({
       name: appName,
-      // eslint-disable-next-line @typescript-eslint/camelcase
       head_sha: github.context.payload.pull_request?.head.sha,
       status: 'completed',
-      conclusion: isTaskCompleted ? 'success' : 'failure',
-      // eslint-disable-next-line @typescript-eslint/camelcase
+      conclusion,
       completed_at: new Date().toISOString(),
       output: {
         title: appName,
-        summary: isTaskCompleted
-          ? 'All tasks are completed!'
-          : 'Some tasks are uncompleted!',
-        text
+        // summary: isTaskCompleted
+        //   ? 'All tasks are completed!'
+        //   : 'Some tasks are uncompleted!',
+        summary: 'Results',
+        // text
+        text: summary
       },
       owner: github.context.repo.owner,
       repo: github.context.repo.repo
     })
-    if (!isTaskCompleted) {
-      throw new Error('Not all tasks completed')
-    }
+    // if (!isTaskCompleted) {
+    //   throw new Error('Not all tasks completed')
+    // }
   } catch (error) {
     const message =
       ((error as unknown) as Error)?.message || error || 'Something went wrong'
